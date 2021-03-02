8d108475a2e43f4cd63304a44f664e568f3dea14
Derick <deehope00@gmail.com>
2021-03-02 11:23:08 -0300

    aula 03-02-2021 - conversor de temperatura

---
+++ CELSIUS/src/Celsius/Converter_celsius_para_Fahrenheit.java
@@ -0,0 +1,22 @@
+package Celsius;
+
+public class Converter_celsius_para_Fahrenheit {
+
+public static void main(String[] args) {
+
+ float celsius;
+ float Fahrenheit;
+ 
+
+ celsius = 30f;
+ Fahrenheit = ((9*celsius) + 160)/5;
+ 
+ System.out.println("o resultado é: " + Fahrenheit + " Fahrenheit");
+ 
+		 
+		 
+ 
+ 
+	
+}
+}
---
+++ TESTE1/src/retangulo/Retangulo.java
@@ -0,0 +1,24 @@
+package retangulo;
+
+public class Retangulo {
+
+
+	public static void main(String[] args) {
+	
+		double altura;
+		double base;
+		double area;
+		double perimetro;
+		
+		base = 10;
+		altura = 4;
+		area = base * altura;
+		perimetro = altura + base + altura + base;
+		
+		System.out.println("Área " + area + " metros");
+		System.out.println("Perimetro " + area + " metros" );
+		
+				
+	}
+
+}
